e2f7fc35509d1fe45afa0349974e43cab9090748
Kamal Arumugam <kamalak1506@gmail.com>
2026-06-14 10:33:22 +0530

    Middle Element in LL

---
+++ middleElementLL.java
@@ -0,0 +1,24 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+} */
+
+class Solution {
+    int getMiddle(Node head) {
+        // code here
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.data;
+    }
+}
+https://www.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1
